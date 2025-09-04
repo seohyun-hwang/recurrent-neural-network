a47bc93f333ac104c134047985590146c8753e09
seohyun-hwang <seohyun.hwang07@gmail.com>
2025-09-04 17:37:16 -0400

    Version 1.1
    Still incomplete
    - Updated README
    - Updated a few comments
    - Implemented mean-squared-error error calculation
    - Added an outline for next steps in development

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-PROJECT STILL IN PROGRESS! 
+PROJECT STILL IN PROGRESS! <br>
 <b>Description:</b> Artificial Neural Network in the C programming language; employs the ReLU and Softmax activation-functions. Does not rely on Scikit-learn.
 
-Currently implemented: forward pass, activation functions (ReLU, Softmax)
+Currently implemented: forward pass, activation functions (ReLU, Softmax), Mean-Squared-Error error calculation
 
 Thank you for your attention.
 ~ Seohyun
--- main.c
+++ main.c
@@ -8,20 +8,23 @@ int main(void) {
     int total_layerCount;
 
     int layer_firstElementPointer[total_layerCount]; // all layers incl. the input layer and incl. the position immediately after total_neuronCount
+    int outputLayer_neuronCount = total_neuronCount - layer_firstElementPointer(total_layerCount - 1);
 
     float weight[total_neuronCount];
     float bias[total_neuronCount];
     float output[total_neuronCount];
 
+    float desiredOutput[outputLayer_neuronCount];
 
-    for (int i = 0; i < total_neuronCount; i++) { // setting each neuron's weight and bias to default
+
+    for (int i = 0; i < total_neuronCount; i++) { // setting each neuron's weight and bias to default-values
         weight(i) = 1;
         bias(i) = 0;
     }
 
-    //sets to each neuron a nonactivated output-value
+    //sets to each neuron a ReLU-activated output-value
     for (int i = 1; i < total_layerCount; i++) { // skips to the next layer; starts at layer 1 (excl. input-layer which is layer 0)
-        for (int j = layer_firstElementPointer(i); j < layer_firstElementPointer(i + 1); j++) { // looks at each neuron
+        for (int j = layer_firstElementPointer(i); j < layer_firstElementPointer(i + 1); j++) { // deals with each neuron of the hidden and output layers
             if (j == layer_firstElementPointer(i + 1)) {
                 break;
             }
@@ -31,11 +34,12 @@ int main(void) {
             }
 
             // ReLU activation-function
-            if (sum * weight[j] + bias[j] < 0) {
-                output[j] = 0;
+            float output = sum * weight[j] + bias[j];
+            if (output < 0) {
+                output(j) = 0;
             }
             else {
-                output[j] = sum * weight[j] + bias[j];
+                output(j) = output;
             }
         }
     }
@@ -49,7 +53,32 @@ int main(void) {
         output[i] = expf(output[i]) / zjSum; // Softmax completed
     }
 
-    // loss check
+    // error calculation using Mean Squared Error
+    float mse = 0;
+    for (int i = layer_firstElementPointer(total_layerCount - 1); i < outputLayer_neuronCount; i++) { // integration of denominator into the whole function
+        mse += (output[i] - desiredOutput[i]) * (output[i] - desiredOutput[i]);
+    }
+    mse /= outputLayer_neuronCount;
+
+
+    // computing the negative error gradient through backpropagation chain rule
+
+    // float errorGradient = 2 * currentLayer_effectSumWrtPrevOutput
+    // float currentLayer_effectSum = wjL * ReLU'(zjL) * (ReLU(zjL) - yj)
+
+    // computing the gradient descent and adjusting weights/biases accordingly
+
+    // previousLayerNeuron_desiredOutput = ak - currentLayer_effectSumWrtPrevOutput
+    // ak = output(i - 1)?
+
+
+
+    // repeat over and over with swapped training input-values
+
+
+    // evaluate the model using testing input-values
+
+    // display the average Mean Squared Error across all tests, then end program
 
 
     return 0;
